aa64947d152ce74476768cc61bd985fd322ce921
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-07-08 21:11:06 -0400

    Adding name

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
     return (
       <div id="wd-dashboard">
         <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-        <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+        <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
         <div id="wd-dashboard-courses">
           <div className="wd-dashboard-course">
           <img src="/images/reactjs.webp" width={200} alt="React logo" />
@@ -21,91 +21,91 @@ export default function Dashboard() {
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/3442/Home">
+                CS3442 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/3442/Home"> Go </a>
             </div>
           </div>
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/6543/Home">
+                CS6543 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/6543/Home"> Go </a>
             </div>
           </div>
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/2453/Home">
+                CS2453 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/2453/Home"> Go </a>
             </div>
           </div>
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/3434/Home">
+                CS3434 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/3434/Home"> Go </a>
             </div>
           </div>
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/3435/Home">
+                CS3435 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/3435/Home"> Go </a>
             </div>
           </div>
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/4545/Home">
+                CS4545 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/4545/Home"> Go </a>
             </div>
           </div>
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.webp" width={200} />
             <div>
               <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
+                href="#/Kanbas/Courses/8384/Home">
+                CS8384 React JS
               </a>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              <a href="#/Kanbas/Courses/8384/Home"> Go </a>
             </div>
           </div>
           
