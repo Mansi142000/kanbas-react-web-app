27dbffad2227230257d525ca2856e4d2709dc52e
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-07-22 15:23:16 -0400

    Grades and Assignment done

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -3,8 +3,11 @@ import AssignmentControlButtons from "./AssignmentControlButtons";
 import AsignmentButtons from "./AsignmentButtons";
 import { BsGripVertical } from "react-icons/bs";
 import { MdOutlineArrowDropDown, MdOutlineLibraryBooks } from "react-icons/md";
+import { useParams } from "react-router";
+import { assignments } from "../../Database";
 
 export default function Assignment() {
+  const { cid } = useParams();
   return (
     <div>
       <style>
@@ -24,59 +27,27 @@ export default function Assignment() {
         <br />
         <br />
         <ul id="wd-modules" className="list-group rounded-0">
+        {assignments
+          .filter((assignment: any) => assignment.course === cid)
+          .map((assignment: any) => (
           <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
             <div className="wd-title p-3 ps-2 bg-secondary">
               <BsGripVertical className="me-2 fs-3" />
+              {assignment.name}
               <MdOutlineArrowDropDown />
               <b>ASSIGNMENTS</b>
               <AssignmentControlButtons />
             </div>
             <ul className="wd-lessons list-group rounded-0">
-              <a
-                className="wd-assignment-link"
-                href="#/Kanbas/Courses/1234/Assignments/123"
-              >
                 <li className="wd-lesson list-group-item p-3 ps-1">
                   <BsGripVertical className="me-2 fs-3" />
-                  <MdOutlineLibraryBooks />
-                  <span className="red-text">Multiple Modules</span> |{" "}
-                  <b>Not available until</b> May 6 at 12:00 am | <br></br>{" "}
-                  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
-                  <b>Due</b> May 12 at 11:59pm | 100 pts
+                  {assignment.title}
                   <AsignmentButtons />
                 </li>
-              </a>
-              <a
-                className="wd-assignment-link"
-                href="#/Kanbas/Courses/1234/Assignments/123"
-              >
-                <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <MdOutlineLibraryBooks />
-                  <span className="red-text">Multiple Modules</span> |{" "}
-                  <b>Not available until</b> May 13 at 12:00 am | <br></br>{" "}
-                  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
-                  <b>Due</b> May 20 at 11:59pm | 100 pts
-                  <AsignmentButtons />
-                </li>
-              </a>
-              <a
-                className="wd-assignment-link"
-                href="#/Kanbas/Courses/1234/Assignments/123"
-              >
-                <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <MdOutlineLibraryBooks />
-                  <span className="red-text">Multiple Modules</span> |{" "}
-                  <b>Not available until</b> May 20 at 12:00 am | <br></br>{" "}
-                  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
-                  <b>Due</b> May 27 at 11:59pm | 100 pts
-                  <AsignmentButtons />
-                </li>
-              </a>
             </ul>
           </li>
-        </ul>
+        ))}
+      </ul>
       </div>
     </div>
   );
--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -1,31 +1,67 @@
+import React, { useEffect, useState } from 'react';
 import { FaFileImport } from "react-icons/fa";
 import { LiaFileImportSolid } from "react-icons/lia";
 import { IoMdSettings } from "react-icons/io";
-import { CiSearch, CiFilter } from "react-icons/ci";
+import { CiFilter } from "react-icons/ci";
+
+import usersData from '../../Database/users.json';
+import gradesData from '../../Database/grades.json';
+
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface Grade {
+  _id: string;
+  student: string;
+  assignment: string;
+  grade: string;
+}
 
 export default function Grades() {
+  const [students, setStudents] = useState<User[]>([]);
+  const [grades, setGrades] = useState<Grade[]>([]);
+  const [assignmentNames, setAssignmentNames] = useState<string[]>([]);
+
+  useEffect(() => {
+    setStudents(usersData as User[]);
+    setGrades(gradesData as Grade[]);
+    const uniqueAssignments = Array.from(new Set(gradesData.map(grade => grade.assignment)));
+    setAssignmentNames(uniqueAssignments);
+  }, []);
+
+  const getStudentGrades = (studentId: string) => {
+    const studentGrades = grades.filter(grade => grade.student === studentId);
+    return assignmentNames.map((assignmentName) => {
+      const grade = studentGrades.find(g => g.assignment === assignmentName);
+      return <td key={`${studentId}-${assignmentName}`}>{grade ? `${grade.grade}%` : '0%'}</td>;
+    });
+  };
+
   return (
     <div className="container">
       <div className="row justify-content-end">
         <div className="col-auto pe-1">
-          <button id="wd-collapse-all-1" className="btn btn-lg btn-secondary">
+          <button className="btn btn-lg btn-secondary">
             <FaFileImport />
             Import
           </button>
         </div>
         <div className="col-auto px-1">
-          <button id="wd-collapse-all-2" className="btn btn-lg btn-secondary">
+          <button className="btn btn-lg btn-secondary">
             <LiaFileImportSolid />
             Export
           </button>
         </div>
         <div className="col-auto ps-1">
-          <button id="wd-collapse-all-3" className="btn btn-lg btn-secondary">
+          <button className="btn btn-lg btn-secondary">
             <IoMdSettings />
           </button>
         </div>
       </div>
-      <div className="row  mt-3">
+      <div className="row mt-3">
         <div className="col m-2">
           <div className="row">
             <div className="col">
@@ -38,7 +74,7 @@ export default function Grades() {
             <div className="col border border-dark rounded p-1">
               <input
                 className="border-0"
-                placeholder="&#x1F50E; Search Stundents"
+                placeholder="&#x1F50E; Search Students"
               />
             </div>
           </div>
@@ -61,7 +97,7 @@ export default function Grades() {
           </div>
         </div>
         <div className="col-12">
-          <button id="wd-filter-btn" className="btn btn-secondary">
+          <button className="btn btn-secondary">
             <CiFilter />
             Apply Filter
           </button>
@@ -71,56 +107,18 @@ export default function Grades() {
             <thead>
               <tr>
                 <th scope="col">Student Name</th>
-                <th scope="col">A1 SETUP <br/> Out of 100</th>
-                <th scope="col">A2 HTML <br/> Out of 100</th>
-                <th scope="col">A3 CSS<br/> Out of 100</th>
-                <th scope="col">A4 BOOTSTRAP<br/> Out of 100</th>
+                {assignmentNames.map((name) => (
+                  <th key={name} scope="col">{name}</th>
+                ))}
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row" className="text-danger">Jane Adams</th>
-                <td>100%</td>
-                <td>78.6%</td>
-                <td>89.5%</td>
-                <td>95.6%</td>
-              </tr>
-              <tr>
-                <th scope="row" className="text-danger">Simone Wanner</th>
-                <td>100%</td>
-                <td className="d-flex align-items-center">
-                  <input
-                    type="text"
-                    className="form-control me-2"
-                    defaultValue="100%"
-                    style={{ width: "80px" }}
-                  />
-                  <LiaFileImportSolid />
-                </td>
-                <td>85.6%</td>
-                <td>97.6%</td>
-              </tr>
-              <tr>
-                <th scope="row" className="text-danger">Deep Patel</th>
-                <td>100%</td>
-                <td>85.7%</td>
-                <td>93.6%</td>
-                <td>100%</td>
-              </tr>
-              <tr>
-                <th scope="row" className="text-danger">Akshay Singh</th>
-                <td>100%</td>
-                <td>97.4%</td>
-                <td>95.3%</td>
-                <td>89.4%</td>
-              </tr>
-              <tr>
-                <th scope="row" className="text-danger">Matt Williams</th>
-                <td>92.1%</td>
-                <td>95.7%</td>
-                <td>100%</td>
-                <td>94.3%</td>
-              </tr>
+              {students.map(student => (
+                <tr key={student._id}>
+                  <th scope="row" className="text-danger">{`${student.firstName} ${student.lastName}`}</th>
+                  {getStudentGrades(student._id)}
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
