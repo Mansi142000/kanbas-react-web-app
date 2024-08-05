85bff508d6d36aef47f5fb185a85638a07c8aa84
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-08-05 13:55:06 -0400

    fixed bug - unable to see course name

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -3,54 +3,65 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Navigate, Route, Routes, useLocation, useParams } from "react-router";
-import { FaAlignJustify } from "react-icons/fa6";
+import { Navigate, Route, Routes, useLocation, useParams } from "react-router-dom"; // Fixed import for react-router
+import { FaAlignJustify } from "react-icons/fa";
 import Grades from "./Grades";
-import { courses } from "../Database";
+import { fetchCourse } from "./client"; // Ensure this is correctly imported
 import AssignmentAdder from "./Assignments/Adder";
 import Quizzes from "./Quizzes";
 import QuizEditor from "./Quizzes/Editor";
 import QuizAdder from "./Quizzes/Adder";
 import { useState, useEffect } from "react";
-import { fetchCourse } from "./client";
+
 export default function Courses() {
   const { cid } = useParams();
   const { pathname } = useLocation();
   const [course, setCourse] = useState(null);
-
+  const [loading, setLoading] = useState(true); // Added loading state
+  const [error, setError] = useState(null); // Added error state
   useEffect(() => {
-    setCourse(fetchCourse(cid));
-  }, [cid]);
+    // Define the async function inside useEffect
+    const loadCourse = async () => {
+      try {
+        const fetchedCourse = await fetchCourse(cid);
+        setCourse(fetchedCourse);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  if (!course) {
-    return <div>Loading...</div>;
-  } else
-    return (
-      <div id="wd-courses" className="container">
-        <div className="row">
-          <h2 className="text-danger">
-            <FaAlignJustify className="me-4 fs-4 mb-1" />
-            {course && course.name} &gt; {pathname.split("/")[4]}
-          </h2>
-          <hr />
-          <div className="col col-2">
-            <CoursesNavigation />
-          </div>
-          <div className="col col-10">
-            <Routes>
-              <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<Assignments />} />
-              <Route path="Quizzes" element={<Quizzes />} />
-              <Route path="Quizzes/:qid/QuizEdit" element={<QuizEditor />} />
-              <Route path="AddQuiz" element={<QuizAdder />} />
-              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-              <Route path="AddAssignment" element={<AssignmentAdder />} />
-              <Route path="Grades" element={<Grades />} />
-            </Routes>
-          </div>
+    loadCourse();
+  }, [cid]);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading course: {error.message}</div>;
+  return (
+    <div id="wd-courses" className="container">
+      <div className="row">
+        <h2 className="text-danger">
+          <FaAlignJustify className="me-4 fs-4 mb-1" />
+          {course && course.name} &gt; {pathname.split("/")[4]}
+        </h2>
+        <hr />
+        <div className="col col-2">
+          <CoursesNavigation />
+        </div>
+        <div className="col col-10">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Quizzes" element={<Quizzes />} />
+            <Route path="Quizzes/:qid/QuizEdit" element={<QuizEditor />} />
+            <Route path="AddQuiz" element={<QuizAdder />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+            <Route path="AddAssignment" element={<AssignmentAdder />} />
+            <Route path="Grades" element={<Grades />} />
+          </Routes>
         </div>
       </div>
-    );
+    </div>
+  );
 }
