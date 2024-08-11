7c1f31b755ab31a0f2b9a29d1b4daf6eb4e99e34
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-08-11 11:17:26 -0400

    final push prolly

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,6 +5,8 @@ import Courses from "./Courses";
 import "./styles.css";
 import store from "./store";
 import { Provider } from "react-redux";
+import Account from "./Account";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function Kanbas() {
     return (
@@ -14,9 +16,9 @@ export default function Kanbas() {
                 <div  className="wd-main-content-offset p-3">
                 <Routes>
                     <Route path="/" element={<Navigate to="Dashboard" />} />
-                    <Route  path="Account" element={<><h1>Account</h1></>} />
-                    <Route path="Dashboard" element={<Dashboard />} />
-                    <Route path="Courses/:cid/*" element={<Courses />} />
+                    <Route  path="Account/*" element={<Account/>} />
+                    <Route path="Dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+                    <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses /></ProtectedRoute>} />
                     <Route path="Calendar" element={<><h1>Calendar</h1></>}/>
                     <Route path="Inbox" element={<><h1>Inbox</h1></>}/>
                 </Routes>
