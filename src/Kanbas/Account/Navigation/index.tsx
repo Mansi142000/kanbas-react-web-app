23663b654b4e4625ac749afd73fa0fae60962584
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-08-12 21:57:04 -0400

    Update index.tsx

--- src/Kanbas/Account/Navigation/index.tsx
+++ src/Kanbas/Account/Navigation/index.tsx
@@ -2,7 +2,7 @@ import { Link, useLocation, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = ["Signin", "Signup", "Profile"];
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
   return (
     <div id="wd-account-navigation" className="list-group fs-5 rounded-0">
